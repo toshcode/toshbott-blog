59c323c0205eb0babec73499e96c5b54ca44e5d4
toshcode <gitonjeruh@gmail.com>
2023-08-25 13:01:10 +0300

    create navigation link for posts page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,7 +12,8 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Toshbott Learn's <a href="https://nextjs.org">Next.js!</a>
+          {/* Toshbott Learn's <a href="https://nextjs.org">Next.js!</a> */}
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
