01933935d63ee1003b90c5d9f16faa98dfdc85dd
toshcode <gitonjeruh@gmail.com>
2023-08-25 14:08:53 +0300

    create layout component and implement in first post page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import Script from "next/script";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 
 export default function FirstPost() {
     return (
-    <>
+    <Layout>
         <Head>
             <title>Tosh's First Post</title>
         </Head>
@@ -20,6 +21,6 @@ export default function FirstPost() {
         <h2>
             <Link href="/">Back to Home</Link>
         </h2>
-    </>
+    </Layout>
     );
 }
