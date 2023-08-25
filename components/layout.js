bcab2ff345dca718bb0495a5f989457b1aa3211d
toshcode <gitonjeruh@gmail.com>
2023-08-25 14:18:13 +0300

    implement css modules in layout component

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,5 @@
-export default function Layout({children}) {
-    return <div>{children}</div>;
+import styles from './layout.module.css'
+
+export default function Layout({ children }) {
+    return <div className={styles.container}>{ children }</div>;
 }
